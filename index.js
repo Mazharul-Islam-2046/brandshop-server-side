a946e78dab7b2f7208cb8eacbff74dd9ed299ba4
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-10-19 14:36:07 +0600

    server side created basic set up

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+
+
+
+
+const port = process.env.PORT || 5000;
+
+
+app.get("/", (req, res) => {
+    res.send("Server in running")
+});
+
+
+app.listen(port, ()=> {
+    console.log("server is running");
+})
+  
+    
