b4a5d9df4f1c5362dc784c8eaaa9cc5b7306389c
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-10-19 15:15:29 +0600

    connected with frontend and created user post method

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
 
+app.use(express.json())
+app.use(cors())
+
 const uri = `mongodb+srv://${process.env.userNameDB}:${process.env.password}@cluster0.ypipc9x.mongodb.net/?retryWrites=true&w=majority`;
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,16 +17,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const usersCollection = client.db("brandDB").collection("users");
+
+    // user post method
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -33,24 +47,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-
-
-
-
 const port = process.env.PORT || 5000;
 
-
 app.get("/", (req, res) => {
-    res.send("Server in running")
+  res.send("Server in running");
 });
 
-
-app.listen(port, ()=> {
-    console.log("server is running");
-})
-  
-    
+app.listen(port, () => {
+  console.log("server is running");
+});
