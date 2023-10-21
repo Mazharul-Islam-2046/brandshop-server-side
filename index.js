34d1ffb99ca50dc4fd4fdaed71bc0f61e415672a
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-10-21 21:09:41 +0600

    final server push

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
 
-
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.userNameDB}:${process.env.password}@cluster0.ypipc9x.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,7 +22,42 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const usersCollection = client.db("brandDB").collection("users");
-    const productCollection = client.db("brandDB").collection("product")
+    const productCollection = client.db("brandDB").collection("product");
+    const brandCollection = client.db("brandDB").collection("brandCollection");
+    const myCartCollection = client.db("brandDB").collection("myCart")
+
+
+    // myCart post method
+    app.post("/mycarts", async (req, res) => {
+      const myCart = req.body;
+      const result = await myCartCollection.insertOne(myCart);
+      res.send(result);
+    });
+
+
+    // myCart get method
+    app.get("/mycarts", async (req, res) => {
+      const result = await myCartCollection.find().toArray();
+      res.send(result);
+    });
+
+
+
+
+    // myCart Delete method
+    app.delete("/mycarts/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("delete", id);
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await myCartCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
+
+
 
     // user post method
     app.post("/users", async (req, res) => {
@@ -32,29 +66,60 @@ async function run() {
       res.send(result);
     });
 
-
     // add product (Post)
     app.post("/products", async (req, res) => {
-        const product = req.body;
-        console.log(product);
-        const result = await productCollection.insertOne(product);
-        console.log(result);
-        res.send(result);
-      });
-
-
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
 
     //   add product (get)
     app.get("/products", async (req, res) => {
-        const result = await productCollection.find().toArray()
-        res.send(result);
-      });
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
 
+    // Product get by id
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await productCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
 
+    // product update by id
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = {
+        $set: {
+          brandName: data.brandName,
+          type: data.type,
+          imageLink: data.imageLink,
+          productName: data.productName,
+          description: data.description,
+          price: data.price,
+          ratings: data.ratings,
+        },
+      };
+      const result = await productCollection.updateOne(
+        filter,
+        updatedProduct,
+        options
+      );
+      res.send(result);
+    });
 
-
-
-
+    // Brand Collection get
+    app.get("/brands", async (req, res) => {
+      const result = await brandCollection.find().toArray();
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
